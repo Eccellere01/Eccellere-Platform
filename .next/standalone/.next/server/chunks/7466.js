87cd59216798862744551e33dc5df33f23f72fbf
Eccellere Dev <admin@eccellere.in>
2026-05-03 15:03:21 +0530

    chore: stop tracking .next build output (Hostinger builds from source)

--- .next/standalone/.next/server/chunks/7466.js
+++
@@ -1,1 +0,0 @@
-"use strict";exports.id=7466,exports.ids=[7466],exports.modules={27466:(a,b,c)=>{c.r(b),c.d(b,{getMachineId:()=>f});var d=c(29021),e=c(17307);async function f(){for(let a of["/etc/machine-id","/var/lib/dbus/machine-id"])try{return(await d.promises.readFile(a,{encoding:"utf8"})).trim()}catch(a){e.s.debug(`error reading machine id: ${a}`)}}}};
